1815bed2ee3fb96f0844e2ccc9410a744cf4cb4a
DrewNow <49587577+DrewNow@users.noreply.github.com>
2020-11-30 14:26:33 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ def readme():
         return f.read()
 
 setup(name='ranking_phase_fields',
-      version='0.1',
+      version='1.0',
       description='Ranking phase fields with likelihood of finding a stable composition',
       url='http://github.com/DrewNow/RankingPhaseFields',
       author='Andrij Vasylenko',
       author_email='and.vasylenko@gmail.com',
       license='MIT',
       packages=['ranking_phase_fields'],
-      install_requires=['pyod','numpy'],
+      install_requires=['pyod','numpy','tensorflow'],
       include_package_data=True,
       entry_points={"console_scripts": ["ranking_phase_fields=ranking_phase_fields.__main__:main"]},  
       zip_safe=False)
