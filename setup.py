5b0a0bd4bb4b4154e527b18d7ed135555e29cfce
DrewNow <49587577+DrewNow@users.noreply.github.com>
2020-12-02 12:22:15 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 def readme():
-    with open('README.rst') as f:
+    with open('README.md') as f:
         return f.read()
 
 setup(name='ranking_phase_fields',
@@ -13,6 +13,7 @@ setup(name='ranking_phase_fields',
       license='MIT',
       packages=['ranking_phase_fields'],
       install_requires=['pyod','numpy','tensorflow'],
+      python_requires='>=3.8, <3.9',
       include_package_data=True,
       entry_points={"console_scripts": ["ranking_phase_fields=ranking_phase_fields.__main__:main"]},  
       zip_safe=False)
